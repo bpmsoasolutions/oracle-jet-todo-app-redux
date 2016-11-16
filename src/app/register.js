e3d48c4113e076808545a076cfde5063c37fe1fc
Daniel Biedma Ramos <info@dbr.io>
2016-11-16 11:15:11 +0100

    add the components and containers to the register

--- src/app/register.js
+++ src/app/register.js
@@ -1,7 +1,15 @@
 import ko from 'knockout';
 
 ko.components.register('router', { require: 'containers/router/router' })
-ko.components.register('default', { require: 'containers/default/default' })
 ko.components.register('bss-nav-bar', { require: 'containers/nav-bar/nav-bar' })
 
 ko.components.register('main-structure', { require: 'components/main-structure/main-structure' })
+ko.components.register('default', { require: 'components/default/default' })
+
+ko.components.register('add-todo', { require: 'containers/add-todo/add-todo' })
+ko.components.register('todo-list', { require: 'containers/todo-list/todo-list' })
+ko.components.register('filters', { require: 'containers/filter-list/filter-list' })
+
+ko.components.register('filter', { require: 'components/filter/filter' })
+ko.components.register('todo', { require: 'components/todo/todo' })
+ko.components.register('add', { require: 'components/add/add' })
