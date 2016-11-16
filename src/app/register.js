14bb843fa37e78158dec6318ebbb3c4dccc95701
Daniel Biedma Ramos <info@dbr.io>
2016-11-16 11:12:43 +0100

    clean base project, remove product component, and style actions, constants reducers

--- src/app/register.js
+++ src/app/register.js
@@ -5,4 +5,3 @@ ko.components.register('default', { require: 'containers/default/default' })
 ko.components.register('bss-nav-bar', { require: 'containers/nav-bar/nav-bar' })
 
 ko.components.register('main-structure', { require: 'components/main-structure/main-structure' })
-ko.components.register('product', { require: 'components/product/product' })
--- src/app/routes.js
+++ src/app/routes.js
@@ -3,11 +3,6 @@ const routes = [{
     container: 'default',
     name: 'Home',
     iconClass: 'fa fa-home fa-2x oj-navigationlist-item-icon'
-},{
-    url: '/product',
-    container: 'product',
-    name: 'Product',
-    iconClass: 'fa fa-product-hunt fa-2x oj-navigationlist-item-icon'
 }]
 
 export default routes
